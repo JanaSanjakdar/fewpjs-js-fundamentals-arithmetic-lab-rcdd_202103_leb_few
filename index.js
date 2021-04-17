0fb227529b5df9f82cd233d6adfc2eb0a3dc59bb
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-17 18:25:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.Write (Multiply(1, 62))*/
 
 let num1 = 32,
 let num2 = 2;
-const Multiply = num1*num2;
+const multiply = num1*num2;
 
 let random= Math.floor(Math.random() * 10) + 1;
 //document.Write(random)
