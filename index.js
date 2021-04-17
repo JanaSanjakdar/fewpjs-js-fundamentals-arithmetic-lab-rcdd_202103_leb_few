2aec79104e8856b1f69515f8b05d282e644bf1cb
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-17 18:09:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+/*  let x=num1;
+  let y=num2;
+  answer=num1*num2;
+  return answer;
+}
+document.Write (Multiply(1, 62))*/
+
+let x= num1, y=num2; let Multiply = num1*num2;
+
+let random= Math.floor(Math.random() * 10) + 1;
+document.Write(random)
+
+function remainder(num3,num4){
+  answer = num3 % num4;
+  return answer;
+}
+document.Write(remainder(16, 6))
+
+let max= Math.max(2,10,18,20)
+document.Write (max)
